591704afa2d122d4a7d990a91194d1e420f31936
unknown <a452457@B035226.ww930.my-it-solutions.net>
2015-12-07 17:47:29 +0100

    Mise en place markers de test, ajout du trafic pour demo, mise en place getLocationRequest a tester

--- app/src/main/java/i2/application/bisonfute/demo/MainActivity.java
+++ app/src/main/java/i2/application/bisonfute/demo/MainActivity.java
@@ -1,35 +1,45 @@
 package i2.application.bisonfute.demo;
 
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean isEventsDisplayed;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -42,16 +52,16 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        setupDrawerLayout(drawer, toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        setupDrawerLayout();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        setupDrawerContent(navigationView);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent();
 
         setupMap();
     }
 
-    private void setupDrawerLayout(DrawerLayout drawer, Toolbar toolbar) {
+    private void setupDrawerLayout() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -59,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void setupDrawerContent(NavigationView navigationView) {
+    private void setupDrawerContent() {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -74,22 +84,68 @@ public class MainActivity extends AppCompatActivity {
     public void selectDrawerItem(MenuItem menuItem) {
         int id = menuItem.getItemId();
 
-        if (id == R.id.nav_location) {
-
-        } else if (id == R.id.nav_traffic) {
+        if (id == R.id.nav_traffic) {
+            displayTraffic();
 
         } else if (id == R.id.nav_events) {
+            displayEvents(!this.isEventsDisplayed);
 
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_settings) {
 
         }
-//        else if (id == R.id.nav_send) {
-//
-//        }
+
+        if (id != R.id.nav_settings) {
+            menuItem.setChecked(!menuItem.isChecked());
+
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+    }
+
+    private void displayTraffic() {
+        mMap.setTrafficEnabled(!mMap.isTrafficEnabled());
+    }
+
+    private void displayEvents(boolean isEventsDisplayed) {
+        if (isEventsDisplayed) {
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.8417531, -0.5691530999999941))
+                            .title("Miroir d'eau")
+                            .snippet("Miroir d'eau de bordeaux")
+            );
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.844469, -0.5737920000000258))
+                            .title("Quinconces")
+                            .snippet("Place des quinconces")
+            );
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.8268502, -0.5560778999999911))
+                            .title("Gare Saint Jean")
+                            .snippet("Gare de Bordeaux")
+            );
+
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.83731700000001, -0.5771620000000439))
+                            .title("Gare Saint Jean")
+                            .snippet("Gare de Bordeaux")
+            );
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.83731700000001, -0.5771620000000439))
+                            .title("HDV")
+                            .snippet("Hote de ville de Bordeaux")
+            );
+            mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(44.830013, -0.5963850000000548))
+                            .title("Stade Chaban Delmas")
+                            .snippet("Stade de Bordeaux")
+            );
+        } else {
+            mMap.clear();
+        }
+        this.isEventsDisplayed = !this.isEventsDisplayed;
+
     }
 
     private void setupMap() {
@@ -107,15 +163,70 @@ public class MainActivity extends AppCompatActivity {
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
                 // Add a marker in Sydney and move the camera
-                LatLng bordeaux = new LatLng(44.8404400 ,-0.5805000);
-                mMap.addMarker(new MarkerOptions().position(bordeaux).title("Marker in Bordeaux"));
-                mMap.getUiSettings().setRotateGesturesEnabled(true);
-                mMap.getUiSettings().setZoomControlsEnabled(true);
-                mMap.setMyLocationEnabled(true);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(bordeaux));
-               // getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mapFragment).commit();
+                setupMapUi();
+                buildGoogleApiClient();
+
             }
         };
+    }
+
+    private void setupMapUi() {
+        LatLng bordeaux = new LatLng(44.8404400, -0.5805000);
+        //mMap.addMarker(new MarkerOptions().position(bordeaux).title("Marker in Bordeaux"));
+        mMap.getUiSettings().setRotateGesturesEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(onMyLocationListener());
+        //mMap.setMyLocationEnabled(true);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bordeaux));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                        // .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(this.getClass().getName(), "Google Api Client Connected");
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, buildLocationRequest(), this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    private LocationRequest buildLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            Log.i(this.getClass().getName(), "Location X : " + location.getLatitude());
+            Log.i(this.getClass().getName(), "Location Y : " + location.getLongitude());
+        }
+    }
+
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationListener() {
+        return
+                new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+                        Log.i(this.getClass().getName(), "LOCATION BUTTON CLICKED.");
+                        return false;
+                    }
+                };
     }
 
 
@@ -145,4 +256,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
